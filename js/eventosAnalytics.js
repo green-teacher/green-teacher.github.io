68f52fb441ce64a0be353e540d9a052fcb31990c
Matias Cano <matiasncano@gmail.com>
2020-10-05 10:56:31 -0300

    Update eventosAnalytics.js
    fix analytics event

--- js/eventosAnalytics.js
+++ js/eventosAnalytics.js
@@ -1,6 +1,6 @@
 function cursosLink(url, origen){
-  gtag('event', 'cursos', {
-   'event_category': origen,
+  gtag('event', origen, {
+   'event_category': 'cursos',
    'event_label': url,
    'event_callback': function(){document.location = url;}
  });
